c0521b042f01d98459270e97cac4d7f3fc24b38a
babariviere <babariviere@protonmail.com>
2018-02-15 10:08:39 +0100

    hotfix

--- src/conv/b.c
+++ src/conv/b.c
@@ -6,19 +6,43 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/14 14:35:27 by briviere          #+#    #+#             */
-/*   Updated: 2018/02/14 14:35:54 by briviere         ###   ########.fr       */
+/*   Updated: 2018/02/15 10:07:52 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_io.h"
 
-char	*conv_b(va_list *ap, t_flags flags)
+static unsigned long long	conv_b_cast(unsigned long long val, t_flag_len len)
+{
+	unsigned long long	casted;
+
+	if (len == FLAG_HH)
+		casted = (unsigned char)val;
+	else if (len == FLAG_H)
+		casted = (unsigned short)val;
+	else if (len == FLAG_NONE)
+		casted = (unsigned int)val;
+	else
+		casted = val;
+	return (casted);
+}
+
+char						*conv_b(va_list *ap, t_flags flags)
 {
 	char		*res;
 	size_t		len;
 
-	res = ft_lltoa_base(va_arg(*ap, unsigned long long), "01");
+	res = ft_lltoa_base(conv_b_cast(va_arg(*ap, unsigned long long),
+				flags.len), "01");
 	len = ft_strlen(res);
-	apply_flags_pad_int_base(&res, len, flags, 'b');
+	if (res[0] == '0' && flags.precision == 0)
+	{
+		res[0] = 0;
+		len = 0;
+	}
+	if (res[0] != '\0' && res[0] != '0')
+		apply_flags_pad_int_base(&res, len, flags, 'b');
+	else
+		apply_flags_pad_int(&res, len, flags, 0);
 	return (res);
 }
